8feaa00dd1b3e8383b060c7fdfff1a7b57cad174
Rodrigo Dumont <rlsdumont@gmail.com>
2017-12-20 12:10:47 -0200

    Add text justification

--- strings/main.go
+++ strings/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -28,21 +29,46 @@ func main() {
 		lineLength := 0
 		line := []string{}
 		for _, word := range strings.Split(p, " ") {
-			if lineLength+len(word)+1 > limit {
-				printLine(line)
+			if lineLength+len(line)+len(word) > limit {
+				printLine(line, limit, lineLength)
 				line = []string{word}
 				lineLength = len(word)
 				continue
 			}
 
 			line = append(line, word)
-			lineLength += len(word) + 1
+			lineLength += len(word)
 		}
 
-		printLine(line)
+		printLine(line, limit, lineLength)
 	}
 }
 
-func printLine(line []string) {
-	fmt.Println(strings.Join(line, " "))
+func printLine(line []string, limit, length int) {
+	if length == 0 {
+		fmt.Println()
+		return
+	}
+
+	spaceCount := limit - length
+	wordSpaceCount := int(math.Trunc(float64(spaceCount / (len(line) - 1))))
+	spaces := strings.Repeat(" ", wordSpaceCount)
+
+	rem := spaceCount % (len(line) - 1)
+	acc := ""
+	for _, w := range line {
+		if acc == "" {
+			acc = w
+			continue
+		}
+
+		if rem > 0 {
+			acc += " "
+			rem--
+		}
+
+		acc += spaces + w
+	}
+
+	fmt.Println(acc)
 }
